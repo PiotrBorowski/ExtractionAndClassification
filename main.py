78709b930e8d8e9400fa78edd2132db9d787263d
PiotrBorowski <piotrborowski97@op.pl>
2020-12-10 19:41:27 +0100

    poprawne testy na mmo i eksperymenty na PON

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ import keras
 import warnings
 from datetime import datetime
 
-
+# Po ludzku na ekranie
+np.set_printoptions(suppress=True, precision=3)
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -49,7 +50,8 @@ synthetic2ClassX, synthetic2ClassY = make_classification(
     n_samples=1000,
     n_classes=2,
     n_features=5000,
-    random_state=1410
+    random_state=1410,
+    n_informative=2500
 )
 
 synthetic3ClassX, synthetic3ClassY = make_classification(
@@ -57,7 +59,8 @@ synthetic3ClassX, synthetic3ClassY = make_classification(
     n_classes=3,
     n_clusters_per_class=1,
     n_features=5000,
-    random_state=1410
+    random_state=1410,
+    n_informative=2500
 )
 
 synthetic2ClassUnbalancedX, synthetic2ClassUnbalancedY = make_classification(
@@ -67,7 +70,8 @@ synthetic2ClassUnbalancedX, synthetic2ClassUnbalancedY = make_classification(
     n_clusters_per_class=2,
     n_features=5000,
     n_redundant=2,
-    random_state=1410
+    random_state=1410,
+    n_informative=2500
 )
 
 synthetic3ClassUnbalancedX, synthetic3ClassUnbalancedY = make_classification(
@@ -76,8 +80,20 @@ synthetic3ClassUnbalancedX, synthetic3ClassUnbalancedY = make_classification(
     n_clusters_per_class=1,
     weights=[0.1, 0.6, 0.3],
     n_features=5000,
-    random_state=1410
+    random_state=1410,
+    n_informative=2500
 )
+
+def createData(features):
+    x, y = make_classification(
+        n_samples=1000,
+        n_classes=2,
+        n_features=features,
+        random_state=1410,
+        n_informative=round((features/2))
+    )
+    return x,y
+
 ##############
 
 # print(attributes, )
@@ -94,10 +110,13 @@ gnb = GaussianNB()
 cart = DecisionTreeClassifier(random_state=0)
 svc = SVC()
 mlp = MLPClassifier()
+mlp2 = MLPClassifier(hidden_layer_sizes=(100, 100))
+mlp3 = MLPClassifier(hidden_layer_sizes=(100,100,100))
+mlp4 = MLPClassifier(hidden_layer_sizes=(100,100,100,100))
 
 
 # + 2 inne sieci
-clfs = [knn, gnb, cart, svc, mlp ]
+clfs = [knn, gnb, cart, svc, mlp, mlp2, mlp3, mlp4 ]
 
 clfsResults = []
 clfsResultsBalanced = []
@@ -150,17 +169,19 @@ extractedXPCA = pca.transform(X)
 
 Xs = [X, extractedXPCA]
 
-for _, clf in enumerate(clfs):
-    print_to_file(clf)
-    for i, x in enumerate(Xs):
-        print_to_file(i)
+for i, x in enumerate(Xs):
+    print_to_file(i)
+    for _, clf in enumerate(clfs):
+        print_to_file(clf)
         makeExperiment(x, y, clf)
 
 def test(x):
     t_statistic = np.zeros((len(clfs), len(clfs)))
     p_value = np.zeros((len(clfs), len(clfs)))
+    print(p_value.shape, len(x))
     for i in range(len(clfs)):
         for j in range(len(clfs)):
+            print((i,j))
             t_statistic[i, j], p_value[i, j] = ttest_ind(x[i], x[j])
 
     return t_statistic, p_value
@@ -168,42 +189,59 @@ def test(x):
 
 
 def printTest(t, p):
-    headers = ['knn', 'gnb', 'cart', 'svc', 'mlp' ]
-    names_column = [['knn'], ['gnb'], ['cart'], ['svc'], ['mlp'] ]
+    headers = ['knn', 'gnb', 'cart', 'svc', 'mlp', 'mlp2', 'mlp3', 'mlp4' ]
+    names_column = [['knn'], ['gnb'], ['cart'], ['svc'], ['mlp'],  ['mlp2'],  ['mlp3'],  ['mlp4'] ]
     t_statistic_table = np.concatenate((names_column, t), axis=1)
     t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
     p_value_table = np.concatenate((names_column, p), axis=1)
     p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
     print("t-statistic:\n", t_statistic_table, "\n\np-value:\n", p_value_table)
 
+print('bez ekstr')
+printTest(*test(clfsResults[:8]))
+print('z ekstr')
+printTest(*test(clfsResults[8:]))
+print('----------BALANCE_ACCURACY-----------')
+print('bez ekstr')
+printTest(*test(clfsResultsBalanced[:8]))
+print('z ekstr')
+printTest(*test(clfsResultsBalanced[8:]))
 
-printTest(*test(clfsResults))
-print('---------------------')
-printTest(*test(clfsResultsBalanced))
-
-exit()
 # dane syntetyczne PON
 Xs = [synthetic2ClassX, synthetic3ClassX, synthetic2ClassUnbalancedX, synthetic3ClassUnbalancedX]
 ys = [synthetic2ClassY, synthetic3ClassY, synthetic2ClassUnbalancedY, synthetic3ClassUnbalancedY]
 
-# bez ekstrakcji
-for i in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]:
-    for i, x in enumerate(Xs):
-        print_to_file(i)
-        # for _, clf in enumerate(clfs):
-        #     print(clf)
-        #     makeExperiment(x, ys[i], clf)
-        print_to_file('mlp')
-        model = build_mlp(x,ys[i])
-        makeExperiment(x, ys[i], model)
 
-    # z ekstrakcja
+
+for i in [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]:
+    xx, yy = createData(i)
+    print_to_file(i)
     for _, clf in enumerate(clfs):
         print_to_file(clf)
-        for i, x in enumerate(Xs):
-            print_to_file(i)
-            print_to_file('EXTRACTED')
-            pca = PCA(n_components=100)
-            pca.fit(x[:round(len(x) * 0.8)])
-            extractedX = pca.transform(x);
-            makeExperiment(extractedX, ys[i], clf)
+        makeExperiment(xx, yy, clf)
+        print_to_file('EXTRACTED')
+        pca = PCA(n_components=round(0.25 * i))
+        pca.fit(xx[:round(len(xx) * 0.8), :i])
+        extractedX = pca.transform(xx);
+        makeExperiment(extractedX, yy, clf)
+
+
+# bez ekstrakcji
+print_to_file(i)
+for _, clf in enumerate(clfs):
+    print_to_file(clf)
+    for ix, x in enumerate(Xs):
+        print_to_file(ix)
+        makeExperiment(x[:,:i], ys[ix], clf)
+
+
+# z ekstrakcja
+for _, clf in enumerate(clfs):
+    print_to_file(clf)
+    for ix, x in enumerate(Xs):
+        print_to_file(ix)
+        print_to_file('EXTRACTED')
+        pca = PCA(n_components=round(0.25 * i))
+        pca.fit(x[:round(len(x) * 0.8), :i])
+        extractedX = pca.transform(x[:,:i]);
+        makeExperiment(extractedX, ys[ix], clf)
